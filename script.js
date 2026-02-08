6c6f915736f3722a6914fd85abbe4c80677583fa
Natália Rafaela <natalia.rafaela33@icloud.com>
2026-02-08 20:28:54 -0300

    Update script.js
    botão back to top

--- script.js
+++ script.js
@@ -189,3 +189,14 @@ Tamanho: ${tamanho}`;
     `https://wa.me/${numero}?text=${encodeURIComponent(msg)}`
   );
 }
+const backToTop = document.getElementById("backToTop");
+
+window.addEventListener("scroll", () => {
+  backToTop.style.display = window.scrollY > 300 ? "block" : "none";
+});
+
+backToTop.addEventListener("click", () => {
+  window.scrollTo({ top: 0, behavior: "smooth" });
+});
+
+
